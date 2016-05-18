c3a6b660cf2fb6af2213ed4e5e949a4358e20cf3
Jakub Wilk <jwilk@jwilk.net>
2016-05-18 16:19:26 +0200

    pydiatra.astaux: add docstring.

--- pydiatra/astaux.py
+++ pydiatra/astaux.py
@@ -1,6 +1,6 @@
 # encoding=UTF-8
 
-# Copyright © 2011-2014 Jakub Wilk <jwilk@jwilk.net>
+# Copyright © 2011-2016 Jakub Wilk <jwilk@jwilk.net>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the “Software”), to deal
@@ -19,6 +19,10 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+'''
+pydiatra: helper functions for AST manipulation
+'''
 
 import sys
 
