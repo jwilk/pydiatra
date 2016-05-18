7db06b1459ddc0608047cae0dabf886d237a7bf9
Jakub Wilk <jwilk@jwilk.net>
2016-05-18 16:24:40 +0200

    pydiatra.tags: add docstring.

--- pydiatra/tags.py
+++ pydiatra/tags.py
@@ -20,6 +20,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+'''
+pydiatra tags
+'''
+
 class Tag(object):
 
     def __init__(self, path, lineno, *args):
