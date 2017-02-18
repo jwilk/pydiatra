jwilk/pydiatra

commit ba9a174fdcce1040d3a95840b938110f05681dd6

pydiatra.check_re: check also substitution patterns.

pydiatra/check_re.py
@@ -245,16 +245,21 @@ def check(owner, node):
     pattern = args.get('pattern')
     if not isinstance(pattern, (unicode, str, bytes)):
         return
+    repl = args.get('repl')
     flags = args.get('flags', 0)
     if not isinstance(flags, int):
         return
     if func_name == 'template':
         flags |= re.TEMPLATE
     flags &= ~re.DEBUG
+    check_sub = func_name.startswith('sub') and isinstance(repl, (unicode, str, bytes))
     try:
         with warnings.catch_warnings(record=True) as wrns:
             warnings.simplefilter('default')
-            re.compile(pattern, flags=flags)
+            if check_sub:
+                re.sub(pattern, repl, pattern[:0], flags=flags)
+            else:
+                re.compile(pattern, flags=flags)
         subpattern = sre_parse.parse(pattern, flags=flags)
     except Exception as exc:  # pylint: disable=broad-except
         yield owner.tag(node.lineno, 'regexp-syntax-error', str(exc))

commit f07e0374bcc366b78f5a9017659695f314a5cab5

pydiatra.check_re: for Python 2.6, don't pass flags to re.sub().
The flags argument was added to re.sub() only in 2.7.

Fixes: ba9a174fdcce1040d3a95840b938110f05681dd6

pydiatra/check_re.py
@@ -257,7 +257,14 @@ def check(owner, node):
         with warnings.catch_warnings(record=True) as wrns:
             warnings.simplefilter('default')
             if check_sub:
-                re.sub(pattern, repl, pattern[:0], flags=flags)
+                if sys.version_info < (2, 7):
+                    # The flags argument was added to re.sub() only in 2.7.
+                    if flags == 0:
+                        re.sub(pattern, repl, pattern[:0])
+                    else:
+                        re.compile(pattern, flags=flags)
+                else:
+                    re.sub(pattern, repl, pattern[:0], flags=flags)
             else:
                 re.compile(pattern, flags=flags)
         subpattern = sre_parse.parse(pattern, flags=flags)