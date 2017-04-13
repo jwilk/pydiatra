jwilk/pydiatra

commit 2b4ca5bacc64172d29c2c1c0868a3146a59d898f

setup: acquire long_description from README.

setup.py
@@ -54,6 +54,12 @@ def uopen(*args):
     else:
         return open(*args, encoding='UTF-8')
 
+def get_readme():
+    with io.open('doc/README', encoding='ASCII') as file:
+        content = file.read()
+    content = re.sub('^[.][.] vim:.*', '', content, flags=re.MULTILINE)
+    return '\n' + content.strip() + '\n'
+
 def get_version():
     with io.open('doc/changelog', encoding='UTF-8') as file:
         line = file.readline()
@@ -186,6 +192,7 @@ setup_options = dict(
     version=get_version(),
     license='MIT',
     description='yet another static checker for Python code',
+    long_description=get_readme(),
     classifiers=classifiers,
     url='http://jwilk.net/software/pydiatra',
     author='Jakub Wilk',

commit 9e9111bd5f2939fd0d9f4b898184a8b76e56d510

setup: fix compatibility with Python 2.6.
Fixes: 2b4ca5bacc64172d29c2c1c0868a3146a59d898f

setup.py
@@ -57,7 +57,7 @@ def uopen(*args):
 def get_readme():
     with io.open('doc/README', encoding='ASCII') as file:
         content = file.read()
-    content = re.sub('^[.][.] vim:.*', '', content, flags=re.MULTILINE)
+    content = re.compile('^[.][.] vim:.*', re.MULTILINE).sub('', content)
     return '\n' + content.strip() + '\n'
 
 def get_version():