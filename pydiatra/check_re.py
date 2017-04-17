jwilk/pydiatra

commit 63b93416f46b34a193952edbe2aadf6744061a2a

pydiatra.check_re: introduce RegexpFlag type.

pydiatra/check_re.py
@@ -241,6 +241,9 @@ class ReVisitor(object):
 class BadConst(Exception):
     pass
 
+class RegexpFlag(int):
+    pass
+
 class Evaluator(ast.NodeVisitor):
 
     def generic_visit(self, node):
@@ -257,7 +260,7 @@ class Evaluator(ast.NodeVisitor):
         if isinstance(value, ast.Name) and value.id == 're':
             value = getattr(re, node.attr, None)
             if isinstance(value, int):
-                return value
+                return RegexpFlag(value)
         raise BadConst
 
     def visit_BinOp(self, node):
@@ -271,7 +274,7 @@ class Evaluator(ast.NodeVisitor):
             self.visit(node.left),
             self.visit(node.right),
         ]
-        if all(isinstance(v, int) for v in values):
+        if all(isinstance(v, RegexpFlag) for v in values):
             return op(*values)
         else:
             raise BadConst
@@ -365,7 +368,7 @@ def check(owner, node):
     pattern = args.get('pattern')
     repl = args.get('repl')
     count = args.get('count', args.get('maxsplit'))
-    if isinstance(count, int):
+    if isinstance(count, RegexpFlag):
         yield owner.tag(node, 'regexp-misplaced-flags-argument')
     flags = args.get('flags', 0)
     if not isinstance(pattern, (unicode, str, bytes)):

commit 72ffe5b86b43d08980e269e45338f12ab49e7eef

pydiatra.check_re: fix flag type confusion.
This fixes type of the flags variable when combining more than two
flags.

Fixes: 63b93416f46b34a193952edbe2aadf6744061a2a

pydiatra/check_re.py
@@ -275,7 +275,7 @@ class Evaluator(ast.NodeVisitor):
             self.visit(node.right),
         ]
         if all(isinstance(v, RegexpFlag) for v in values):
-            return op(*values)
+            return RegexpFlag(op(*values))
         else:
             raise BadConst
 
@@ -367,14 +367,14 @@ def check(owner, node):
         args[argnode.arg] = eval_const(argnode.value)
     pattern = args.get('pattern')
     repl = args.get('repl')
-    flags = args.pop('flags', 0)
+    flags = args.pop('flags', RegexpFlag())
     for arg in args.values():
         if isinstance(arg, RegexpFlag):
             yield owner.tag(node, 'regexp-misplaced-flags-argument')
             break
     if not isinstance(pattern, (unicode, str, bytes)):
         return
-    if not isinstance(flags, int):
+    if not isinstance(flags, RegexpFlag):
         return
     for (n1, f1), (n2, f2) in incompatible_flag_pairs:
         if (f1 & flags) and (f2 & flags):

tests/regexp-syntax-error.t
@@ -66,7 +66,7 @@ re.compile('#(\n)', re.VERBOSE | re.MULTILINE)
 ## [<< 3.5] *: regexp-syntax-error unbalanced parenthesis
 ## [>= 3.5] *: regexp-syntax-error unbalanced parenthesis at position 3 (line 2, column 1)
 
-re.compile('#(\n)', flags=(re.VERBOSE + re.DEBUG))
+re.compile('#(\n)', flags=(re.VERBOSE + re.MULTILINE + re.DEBUG))
 ## [<< 3.5] *: regexp-syntax-error unbalanced parenthesis
 ## [>= 3.5] *: regexp-syntax-error unbalanced parenthesis at position 3 (line 2, column 1)
 