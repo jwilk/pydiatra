435ea377801a9a992df868a95aa1b40dc28a9518
Jakub Wilk <jwilk@jwilk.net>
2018-11-28 21:14:12 +0100

    Add Python version check.
    Before:
    
        $ python2.5 ./py2diatra
        .../pydiatra/main.py:40: Warning: 'as' will become a reserved keyword in Python 2.6
        Traceback (most recent call last):
          File "./py2diatra", line 2, in <module>
            import pydiatra.main
          File ".../pydiatra/main.py", line 40
            except ImportError as exc:
                                ^
        SyntaxError: invalid syntax
    
    After:
    
        $ python2.5 ./py2diatra
        Traceback (most recent call last):
          File "./py2diatra", line 2, in <module>
            import pydiatra.main
          File ".../pydiatra/__init__.py", line 5
            b''  # Python >= 2.6 is required
              ^
        SyntaxError: invalid syntax

--- pydiatra/__init__.py
+++ pydiatra/__init__.py
@@ -2,4 +2,11 @@
 yet another static checker for Python code
 '''
 
+import sys
+
+type(b'')  # Python >= 2.6 is required
+if sys.version_info < (3, 2):
+    raise RuntimeError('Python >= 3.2 is required')
+
+__all__ = []
 __version__ = '0.12.5'
