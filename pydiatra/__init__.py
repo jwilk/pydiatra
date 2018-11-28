jwilk/pydiatra

commit 435ea377801a9a992df868a95aa1b40dc28a9518

Add Python version check.
Before:

    $ python2.5 ./py2diatra
    .../pydiatra/main.py:40: Warning: 'as' will become a reserved keyword in Python 2.6
    Traceback (most recent call last):
      File "./py2diatra", line 2, in <module>
        import pydiatra.main
      File ".../pydiatra/main.py", line 40
        except ImportError as exc:
                            ^
    SyntaxError: invalid syntax

After:

    $ python2.5 ./py2diatra
    Traceback (most recent call last):
      File "./py2diatra", line 2, in <module>
        import pydiatra.main
      File ".../pydiatra/__init__.py", line 5
        b''  # Python >= 2.6 is required
          ^
    SyntaxError: invalid syntax

pydiatra/__init__.py
@@ -2,4 +2,11 @@
 yet another static checker for Python code
 '''
 
+import sys
+
+type(b'')  # Python >= 2.6 is required
+if sys.version_info < (3, 2):
+    raise RuntimeError('Python >= 3.2 is required')
+
+__all__ = []
 __version__ = '0.12.5'

commit 0805ad18a04c30e876f1fe304c751298788bdda2

Fix Python version check.
Fixes: 435ea377801a9a992df868a95aa1b40dc28a9518

pydiatra/__init__.py
@@ -5,8 +5,8 @@ yet another static checker for Python code
 import sys
 
 type(b'')  # Python >= 2.6 is required
-if sys.version_info < (3, 2):
-    raise RuntimeError('Python >= 3.2 is required')
+if (3,) < sys.version_info < (3, 2):
+    raise RuntimeError('Python 2.6+ or 3.2+ is required')
 
 __all__ = []
 __version__ = '0.12.5'
0805ad18a04c30e876f1fe304c751298788bdda2
Jakub Wilk <jwilk@jwilk.net>
2018-11-28 21:32:02 +0100

    Fix Python version check.
    Fixes: 435ea377801a9a992df868a95aa1b40dc28a9518

--- pydiatra/__init__.py
+++ pydiatra/__init__.py
@@ -5,8 +5,8 @@ yet another static checker for Python code
 import sys
 
 type(b'')  # Python >= 2.6 is required
-if sys.version_info < (3, 2):
-    raise RuntimeError('Python >= 3.2 is required')
+if (3,) < sys.version_info < (3, 2):
+    raise RuntimeError('Python 2.6+ or 3.2+ is required')
 
 __all__ = []
 __version__ = '0.12.5'
